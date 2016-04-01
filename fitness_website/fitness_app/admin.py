51315697f5775a0d3277e1efbb53e133b7b89367
Sweet <2423651950@qq.com>
2016-04-01 17:02:15 +0800

    insert

--- fitness_website/fitness_app/admin.py
+++ fitness_website/fitness_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from fitness_app.models import People, Source, Project
 
 # Register your models here.
+admin.site.register(People)
+admin.site.register(Source)
+admin.site.register(Project)
--- fitness_website/fitness_app/models.py
+++ fitness_website/fitness_app/models.py
@@ -1,3 +1,43 @@
+# coding:utf-8
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    projectid = models.CharField(max_length=128)
+    projectname = models.CharField(max_length=128)
+    level = models.CharField(max_length=16)
+    time = models.DateField(default=0)
+    interval = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.projectname
+
+
+class People(models.Model):
+    uid = models.AutoField(primary_key=True)  # 用户id
+    name = models.CharField(max_length=128, unique=True)  # 名字
+    age = models.IntegerField(default=12)  # 年龄
+    hight = models.IntegerField(default=0)  # 身高
+    weight = models.IntegerField(default=0)  # 体重
+    aim = models.CharField(max_length=128)  # 目标
+    birth = models.DateField()  # 出生日期
+    projectid = models.ForeignKey(Project)  # 项目id
+    city = models.CharField(max_length=128)  # 城市
+    frequency = models.IntegerField(default=0)  # 次数
+    followed = models.IntegerField(default=0)  # 被点赞
+    like = models.IntegerField(default=0)  # 被关注
+    fans = models.IntegerField(default=0)  # 粉丝数
+    dynamic = models.CharField(max_length=512)  # 动态
+
+    def __unicode__(self):
+        return self.name
+
+
+class Source(models.Model):
+    uid = models.IntegerField(primary_key=True)
+    picpath = models.CharField(max_length=128)
+    text = models.CharField(max_length=512)
+    sendtime = models.DateField()
+
+    def __unicode__(self):
+        return self.text
